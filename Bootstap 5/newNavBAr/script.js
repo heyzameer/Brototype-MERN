0f61d43d477c75f1d2fb65944906a6bf7b0acd90
heyzameer <khanzameer5890@gmail.com>
2025-02-07 15:03:20 +0530

    js unusaual behaviour

--- Bootstap 5/newNavBAr/script.js
+++ Bootstap 5/newNavBAr/script.js
@@ -1,16 +1,19 @@
+
+function onbtn(){
+    var box = document.getElementById("box");
+      box.innerHTML="hello"
+    }
+
+
+
 let box = document.getElementById("box");
 let btn = document.getElementById("btn");
 let state = 0;
 
 btn.addEventListener("click", () => {
    
-        box.style.backgroundColor = "blue";
+        (box.style.backgroundColor == "yellow")? box.style.backgroundColor = "red" : box.style.backgroundColor = "yellow";
 
 });
 
 
-
-function onbtn(){
-    var box = document.getElementById("box");
-      box.innerHTML="hello"
-    }
