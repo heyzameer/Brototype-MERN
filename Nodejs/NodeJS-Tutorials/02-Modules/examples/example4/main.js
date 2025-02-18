e04eebc3ceb8c7228fc75cac761088824e1e6ddf
heyzameer <khanzameer5890@gmail.com>
2025-02-18 10:08:40 +0530

    update modules

--- Nodejs/NodeJS-Tutorials/02-Modules/examples/example4/main.js
+++ Nodejs/NodeJS-Tutorials/02-Modules/examples/example4/main.js
@@ -1,5 +1,5 @@
 // main.js
-const myModule = require('./myModule');
+// const myModule = require('./myModule');
 
 myModule.publicFunction(); // Outputs: "Private counter incremented: 1" and "Public function called"
 myModule.publicFunction(); // Outputs: "Private counter incremented: 2" and "Public function called"
