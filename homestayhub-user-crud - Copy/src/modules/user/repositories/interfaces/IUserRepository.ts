5049a6b80435ec08bb08084955b55875f6ac6a65
heyzameer <119403278+heyzameer@users.noreply.github.com>
2026-01-12 12:58:54 +0530

    Delete homestayhub-user-crud - Copy directory

--- homestayhub-user-crud - Copy/src/modules/user/repositories/interfaces/IUserRepository.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../../model/user.model.js";
-
-export interface IUserRepository {
-  create(data: Partial<IUser>): Promise<IUser>;
-  findById(id: string): Promise<IUser | null>;
-  update(id: string, data: Partial<IUser>): Promise<IUser | null>;
-  delete(id: string): Promise<void>;
-  findByEmail(email: string): Promise<IUser | null>;
-}
